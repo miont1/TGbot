d7679fcbbc97d4f12fe8c12a158ed760d191ef7d
Artem <a.sarain3202@gmail.com>
2025-10-19 22:41:21 +0300

    1st commit

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 from telegram import Update, InlineQueryResultArticle, InputTextMessageContent, ReplyKeyboardMarkup
 from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, InlineQueryHandler
 import os
+from openai import OpenAI
 from dotenv import load_dotenv
 
 
 load_dotenv()
+client = OpenAI()
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
     # keyboard = [
@@ -14,9 +17,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     # await update.message.reply_text("Вибери кнопку:", reply_markup=reply_markup)
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-    print(update.message.from_user.username + " - " + update.message.text)
+# async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+#     print(update.message.from_user.username + " - " + update.message.text)
 
 async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text_caps = ' '.join(context.args).upper()
@@ -26,17 +29,42 @@ async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, this command is not available.")
 
+async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    if text == "Хочу ще факт!":
+        await random(update, context)
+    elif text == "Закінчити":
+        await start(update, context)
+
+
+async def random(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    response = client.responses.create(
+        model="o4-mini",
+        input='Write a random fact'
+    )
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response.output_text)
+    keyboard = [
+        ["Закінчити"],
+        ["Хочу ще факт!"],
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    await update.message.reply_text("Бажаєте ще один факт?", reply_markup=reply_markup)
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
 
     start_handler = CommandHandler('start', start)
     caps_handler = CommandHandler('caps', caps)
+    random_handler = CommandHandler('random', random)
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
-    echo_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
+    button_handler = MessageHandler(filters.TEXT, button_handler)
+    # echo_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
 
     application.add_handler(caps_handler)
     application.add_handler(start_handler)
-    application.add_handler(echo_handler)
+    application.add_handler(random_handler)
+    application.add_handler(button_handler)
+    # application.add_handler(echo_handler)
     application.add_handler(unknown_handler)
 
     application.run_polling()
