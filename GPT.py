6359c5104a8e255f9c3dcdc973a4a7ea034579b0
Artem <a.sarain3202@gmail.com>
2025-10-29 19:05:10 +0200

    /image_describe and /translate added

--- GPT.py
+++ GPT.py
@@ -1,6 +1,8 @@
 from openai import AsyncOpenAI
 from telegram.ext import ContextTypes
 
+from utils import load_prompt
+
 
 class GPT:
     def __init__(self, token):
@@ -16,7 +18,21 @@ class GPT:
 
         message = response.output_text
         self.messages.append({"role": "assistant", "content": message})
+        self.messages.clear()
         return message
+
+    async def send_question(self, prompt_text, question_text):
+        self.messages.clear()
+        self.messages.append({"role": "system", "content": prompt_text})
+        self.messages.append({"role": "user", "content": question_text})
+        return await self.send_messages()
+
+    async def set_prompt(self, prompt):
+        self.messages.clear()
+        self.messages.append({"role": "system", "content": prompt})
+
+    async def send_answer(self, answer):
+        self.messages.append({"role": "system", "content": answer})
 
     async def send_messages_contex(self, context: ContextTypes.DEFAULT_TYPE):
         response = await self.client.responses.create(
@@ -24,7 +40,6 @@ class GPT:
             input=context.user_data["history"],
             max_output_tokens=3000,
         )
-
         message = response.output_text
         context.user_data["history"].append({"role": "assistant", "content": message})
         return message
@@ -34,12 +49,39 @@ class GPT:
         context.user_data["history"].clear()
         context.user_data["history"].append({"role": "system", "content": prompt_text})
 
-    async def add_user_question(self, context, question_text):
+    @staticmethod
+    async def add_user_question(context, question_text):
+        context.user_data["history"].append({"role": "user", "content": question_text})
+
+    async def send_user_question(self, context, question_text):
         context.user_data["history"].append({"role": "user", "content": question_text})
         return await self.send_messages_contex(context)
 
-    async def send_question(self, prompt_text, question_text):
-        self.messages.clear()
-        self.messages.append({"role": "system", "content": prompt_text})
-        self.messages.append({"role": "user", "content": question_text})
-        return await self.send_messages()
+    async def create_photo(self, file_obj):
+        result = await self.client.files.create(
+            file=("photo.jpg", file_obj),
+            purpose="vision",
+        )
+        return result.id
+
+    async def send_question_with_photo(self, prompt_text, file_id):
+        user_input = [
+            {
+                "role": "system",
+                "content": prompt_text},
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "input_image",
+                        "file_id": file_id,
+                    },
+                ],
+            }]
+
+        result = await self.client.responses.create(
+            model="o4-mini",
+            input=user_input,
+            max_output_tokens=3000,
+        )
+        return result.output_text
--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from telegram import Update
 from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
 import os
+import io
 from dotenv import load_dotenv
 
 from GPT import GPT
@@ -30,7 +31,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     check_context(context)
-
     text = update.message.text
 
     if text == "Хочу ще факт!":
@@ -40,7 +40,7 @@ async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     elif context.user_data["mode"] == "chat":
         wait_message = await send_message(update, context, "Даю відповідь на питання...")
         try:
-            answer = await chat_gpt.add_user_question(context, text)
+            answer = await chat_gpt.send_user_question(context, text)
             await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=wait_message.message_id)
             await send_message_with_buttons(update, context, answer, None, ("Закінчити",))
         except Exception as error:
@@ -68,26 +68,65 @@ async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
                                    "Виберіть ім'я відомої особистості зі списку:\n Курт Кобейн\n Єлизавета II\n Джон Толкін\n Фрідріх Ніцше\n Стівен Гокінг")
             else:
                 wait_message = await send_message(update, context, "Даю відповідь на питання...")
-                answer = await chat_gpt.add_user_question(context, text)
+                answer = await chat_gpt.send_user_question(context, text)
                 await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=wait_message.message_id)
                 await send_message_with_buttons(update, context, answer, None, ("Закінчити",))
 
     elif context.user_data["mode"] == "quiz":
         if context.user_data["status"] == "question":
             wait_message = await send_message(update, context, "Думаю над питанням...")
-            answer = await chat_gpt.add_user_question(context, text)
+            answer = await chat_gpt.send_user_question(context, text)
             await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=wait_message.message_id)
             await send_message(update, context, answer)
             context.user_data["status"] = "answer"
 
         elif context.user_data["status"] == "answer":
             wait_message = await send_message(update, context, "Думаю над відповіддю...")
-            answer = await chat_gpt.add_user_question(context, text)
+            answer = await chat_gpt.send_user_question(context, text)
             await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=wait_message.message_id)
             await send_message(update, context, answer)
             await send_message_with_buttons(update, context, "Оберіть тему.", None,
                                             ("Python", "Математика", "Біологія", ("Попередня тема", "Закінчити")))
             context.user_data["status"] = "question"
+
+    elif context.user_data["mode"] == "translate":
+
+        translate_list = {
+            "Українська": "translate_ua",
+            "Англійська": "translate_eng",
+        }
+
+        language = translate_list.get(text)
+        if language:
+            await chat_gpt.set_user_prompt(context, load_prompt(language))
+            await send_message(update, context, "Введіть повідомлення для перекладу:")
+        else:
+
+            wait_message = await send_message(update, context, "Перекладаю...")
+            answer = await chat_gpt.send_user_question(context, text)
+            await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=wait_message.message_id)
+            await send_message_with_buttons(update, context, answer, load_message("translate"),
+                                            (("Українська", "Англійська"), "Закінчити"))
+
+
+async def photo_handler(update, context):
+    if not update.message.photo:
+        await send_message(update, context, "Треба скинути картинку")
+        return
+
+    photo = update.message.photo[-1]
+    tg_file = await context.bot.get_file(photo.file_id)
+    photo_bytes = await tg_file.download_as_bytearray()
+
+    if context.user_data["mode"] == "image_describe":
+        wait_message = await send_message(update, context, "Думаю над відповіддю...")
+        file_obj = io.BytesIO(photo_bytes)
+        file_obj.name = "photo.jpg"
+        file_id = await chat_gpt.create_photo(file_obj)
+
+        response = await chat_gpt.send_question_with_photo(load_prompt("image_describe"), file_id)
+        await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=wait_message.message_id)
+        await send_message(update, context, response)
 
 
 async def quiz(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -147,6 +186,26 @@ async def talk(update: Update, context: ContextTypes.DEFAULT_TYPE):
                                     ("Курт Кобейн", "Єлизавета II", "Джон Толкін", "Фрідріх Ніцше", "Стівен Гокінг"))
 
 
+async def image_describe(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    check_context(context)
+    context.user_data["mode"] = "image_describe"
+
+    photo = load_photo("gpt")
+    await send_photo(update, context, photo)
+    message = load_message("image_describe")
+    await send_message(update, context, message)
+
+
+async def translate(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    check_context(context)
+    context.user_data["mode"] = "translate"
+
+    photo = load_photo("gpt")
+    await send_photo(update, context, photo)
+    message = load_message("translate")
+    await send_message_with_buttons(update, context, message, None, (("Українська", "Англійська"),))
+
+
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await send_message(update, context, "Вибачте, такої команди немає :(")
 
@@ -156,17 +215,23 @@ if __name__ == '__main__':
 
     start_handler = CommandHandler('start', start)
     random_handler = CommandHandler('random', random)
-    button_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler)
+    message_handler_ = MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler)
+    photo_handler_ = MessageHandler(filters.PHOTO, photo_handler)
     gpt_handler = CommandHandler('gpt', gpt)
     talk_handler = CommandHandler('talk', talk)
     quiz_handler = CommandHandler('quiz', quiz)
+    image_describe_handler = CommandHandler('image_describe', image_describe)
+    translate_handler = CommandHandler('translate', translate)
 
     application.add_handler(start_handler)
     application.add_handler(random_handler)
-    application.add_handler(button_handler)
+    application.add_handler(message_handler_)
+    application.add_handler(photo_handler_)
     application.add_handler(gpt_handler)
     application.add_handler(talk_handler)
     application.add_handler(quiz_handler)
+    application.add_handler(image_describe_handler)
+    application.add_handler(translate_handler)
 
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
     application.add_handler(unknown_handler)
